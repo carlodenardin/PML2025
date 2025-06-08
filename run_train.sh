6c27ac1e47019565a1e2af932923b7cf4912b783
Carlo De Nardin <denardincarlo@gmail.com>
2025-06-09 00:23:14 +0200

    Factor VAE

--- run_train.sh
+++ run_train.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 # Test reconstruction error / MIG error for Beta VAE with different beta values
-
+: '
 BETA_VALUES="1 2 4 8 16 32 64"
 
 for beta_value in $BETA_VALUES
 do
   echo "Running Beta VAE (Beta = $beta_value)..."
   python train.py --model_type beta_vae --seed 1234 --beta $beta_value
+done
+'
+
+# Test reconstruction error / MIG error for Factor VAE with different gamma values
+
+GAMMA_VALUES="1 2 4 8 16 32 64"
+
+for gamma in $GAMMA_VALUES
+do
+  echo "Running Beta VAE (Beta = $beta_value)..."
+  python train.py --model_type factor_vae --seed 1234 --gamma $beta_value
 done
