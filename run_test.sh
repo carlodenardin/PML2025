962466772c0dcf8b3f33642adfeb348a3e34755b
Carlo De Nardin <denardincarlo@gmail.com>
2025-06-10 18:05:57 +0200

    Factor VAE

--- run_test.sh
+++ run_test.sh
@@ -27,5 +27,5 @@ CHECKPOINTS_DIR="checkpoints/mpi3d/$MODEL_TYPE"
 for experiment_dir in "$CHECKPOINTS_DIR"/*
 do
     checkpoint_file=$(find "$experiment_dir" -name "*.ckpt" -print -quit)   
-    python test.py --model_type $MODEL_TYPE --checkpoint "$checkpoint_file"     
+    python test.py --model_type $MODEL_TYPE --dataset mpi3d --checkpoint "$checkpoint_file"     
 done
