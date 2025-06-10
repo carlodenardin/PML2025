928a8a60eb69c5485aae495267279d6701aac4bd
Carlo De Nardin <denardincarlo@gmail.com>
2025-06-10 18:04:36 +0200

    Factor VAE

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+: '
 MODEL_TYPE="beta_vae"
 CHECKPOINTS_DIR="checkpoints/dsprites/$MODEL_TYPE"
 
@@ -12,6 +13,16 @@ done
 
 MODEL_TYPE="factor_vae"
 CHECKPOINTS_DIR="checkpoints/dsprites/$MODEL_TYPE"
+
+for experiment_dir in "$CHECKPOINTS_DIR"/*
+do
+    checkpoint_file=$(find "$experiment_dir" -name "*.ckpt" -print -quit)   
+    python test.py --model_type $MODEL_TYPE --checkpoint "$checkpoint_file"     
+done
+'
+
+MODEL_TYPE="beta_vae"
+CHECKPOINTS_DIR="checkpoints/mpi3d/$MODEL_TYPE"
 
 for experiment_dir in "$CHECKPOINTS_DIR"/*
 do
