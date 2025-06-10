d2293e0658e295ae03cbaf9b35f27d5347eca10c
Carlo De Nardin <denardincarlo@gmail.com>
2025-06-10 09:21:04 +0200

    Factor VAE

--- run_train.sh
+++ run_train.sh
@@ -9,8 +9,17 @@ do
   python train.py --model_type beta_vae --seed 19 --beta $beta_value
 done
 '
-
+: '
 GAMMA_VALUES="1 2 4 8 16 32 64"
+
+for gamma_value in $GAMMA_VALUES
+do
+  echo "Running Factor VAE (Gamma = $gamma_value)..."
+  python train.py --model_type factor_vae --seed 19 --gamma $gamma_value
+done
+'
+
+GAMMA_VALUES="16 32 64"
 
 for gamma_value in $GAMMA_VALUES
 do
