98ccaf783245d826f4730e353b8437b9973ac0ad
Carlo De Nardin <denardincarlo@gmail.com>
2025-06-10 18:02:29 +0200

    Factor VAE

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import torch
 from pathlib import Path
 import pytorch_lightning as pl
 from src.dataset import DSpritesDataModule
+from src.mpi3d import MPI3DDataModule
 from src.models.beta_vae import BetaVAE
 from src.models.factor_vae import FactorVAE
 from src.utils import save_reconstructions, compute_mig, get_accelerator, get_seed
@@ -16,6 +17,7 @@ def parse_args():
     parser.add_argument('--num_workers', type = int, default = NUM_WORKERS)
     parser.add_argument('--batch_size', type = int, default = BATCH_SIZE)
     parser.add_argument('--train_val_test_split', type = list, default = TRAIN_VAL_TEST_SPLIT)
+    parser.add_argument('--dataset', type=str, default = 'dsprites', choices = ['dsprites', 'mpi3d'])
     
     return parser.parse_args()
 
@@ -28,21 +30,38 @@ def main():
     print(f"DEVICE: {device}")
 
     
-    dm = DSpritesDataModule(
-        data_dir = DIR_DSPRITES,
-        batch_size = args.batch_size,
-        num_workers = args.num_workers,
-        train_val_test_split = args.train_val_test_split
-    )
+    if args.dataset == 'dsprites':
+        dm = DSpritesDataModule(
+            data_dir=DIR_DSPRITES,
+            batch_size=args.batch_size,
+            num_workers=args.num_workers,
+            train_val_test_split=args.train_val_test_split
+        )
+    elif args.dataset == 'mpi3d':
+        dm = MPI3DDataModule(
+            data_dir = DIR_MPI3D,
+            batch_size = args.batch_size,
+            num_workers = args.num_workers,
+            train_val_test_split = args.train_val_test_split
+        )
+    else:
+        raise ValueError(f"Unknown dataset: {args.dataset}")
     dm.setup(stage = 'test')
+
 
     if args.model_type == 'beta_vae':
         model = BetaVAE.load_from_checkpoint(args.checkpoint)
-        results_dir = Path(RESULTS_DIR_DSPRITES) / args.model_type / result_dir
+        if args.dataset == 'mpi3d':
+            results_dir = Path(RESULTS_DIR_MPI3D) / args.model_type / result_dir
+        else:
+            results_dir = Path(RESULTS_DIR_DSPRITES) / args.model_type / result_dir
         
     if args.model_type == 'factor_vae':
         model = FactorVAE.load_from_checkpoint(args.checkpoint)
-        results_dir = Path(RESULTS_DIR_DSPRITES) / args.model_type / result_dir
+        if args.dataset == 'mpi3d':
+            results_dir = Path(RESULTS_DIR_MPI3D) / args.model_type / result_dir
+        else:
+            results_dir = Path(RESULTS_DIR_DSPRITES) / args.model_type / result_dir
     
     model.eval()
     model.to(device)
