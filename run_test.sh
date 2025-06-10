92b98b3789b678357d27657ceecec2a17c1956cb
Carlo De Nardin <denardincarlo@gmail.com>
2025-06-10 12:05:46 +0200

    Factor VAE

--- run_test.sh
+++ run_test.sh
@@ -9,7 +9,7 @@ do
     python test.py --model_type $MODEL_TYPE --checkpoint "$checkpoint_file"     
 done
 
-. '
+
 MODEL_TYPE="factor_vae"
 CHECKPOINTS_DIR="checkpoints/dsprites/$MODEL_TYPE"
 
@@ -18,4 +18,3 @@ do
     checkpoint_file=$(find "$experiment_dir" -name "*.ckpt" -print -quit)   
     python test.py --model_type $MODEL_TYPE --checkpoint "$checkpoint_file"     
 done
-'
